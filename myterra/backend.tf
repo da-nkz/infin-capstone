78b3a0202ef87dc9bf74b2439dfb19f3ca0e5be2
da-nkz <daniel.ogbonna96@gmail.com>
2024-09-23 12:31:38 +0100

    fix secret issue

--- myterra/backend.tf
+++ myterra/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name   = "another-RG"
     storage_account_name  = "backend2234"
     container_name        = "backend"
-    key = "pmRzjbF61nzYiaNYuYLQpnmcITYhK46KkK935jbdqgd+BS1SOnPG8ioa6ZQ+RLx0GJBIP7sCLZY2+AStyobD7g=="
+    key = "test/terraform.tfstate"
   }
 }
